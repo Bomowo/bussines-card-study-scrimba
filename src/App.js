f7e7a56b6b7a516fb1376124380ca1e498c0efcd
Bomowo <oeccc25@yahoo.com>
2023-07-31 02:39:14 +0300

    Intialize css and fill out Info component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Info from './components/Info';
 import About from './components/About';
 import Interests from './components/Interests.js';
 import Footer from './components/Footer';
+import './style.css'
 
 function App() {
   return (
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,3 +1,13 @@
 export default function Info () {
-    return (<h1>This a test to see "Info" component</h1>)
+    return (<div>
+                <h1>Alexander Os</h1>
+                <h2>Frontend Developer</h2>
+                <p>my.website</p>
+                <a href="mailto: oeccc25@yahoo.com">
+                    <input type="button" value="Email" />
+                </a>
+                <a href="https://www.linkedin.com/in/alexander-osipchuk-b899a01b2" target="_blank"  rel="noreferrer">
+                    <input type="button" value="LinkedIn" />
+                </a>
+            </div>)
 }
