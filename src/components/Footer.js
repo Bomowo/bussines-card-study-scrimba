0fb12f269f5bb92db73b97730ce88ec8a3b413e8
Bomowo <oeccc25@yahoo.com>
2023-07-31 05:43:17 +0300

    Finish css, add photo, minor refactoring, add icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faGithub} from '@fortawesome/free-brands-svg-icons'
+
 export default function Footer () {
-    return (<div>
+    return (<footer>
                 <a href="https://twitter.com/vuperhut" target="_blank"  rel="noreferrer">
-                    <button type="button" value="">Test</button>
+                    <button type="button" value=""><FontAwesomeIcon icon={faTwitter} size="xl" /></button>
                 </a>
                 <a href="https://github.com/Bomowo" target="_blank"  rel="noreferrer">
-                    <button type="button" value="">Test</button>
+                    <button type="button" value=""><FontAwesomeIcon icon={faGithub} size="xl" /></button>
                 </a>
-            </div>)
+            </footer>)
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,13 +1,19 @@
+import avatar from '../res/images/photo_2019-10-29_20-40-06.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 export default function Info () {
-    return (<div>
+    return (<div className='info'>
+                <img src={avatar} alt="my avatar"></img>
                 <h1>Alexander Os</h1>
                 <h3>Frontend Developer</h3>
-                <p>my.website</p>
+                <p>Bomowo</p>
                 <a href="mailto: oeccc25@yahoo.com">
-                    <button type="button" > Email</button>
+                    <button className='email' ><FontAwesomeIcon icon={faEnvelope} /> Email</button>
                 </a>
                 <a href="https://www.linkedin.com/in/alexander-osipchuk-b899a01b2" target="_blank"  rel="noreferrer">
-                    <button type="button" >LinkedIn </button>
+                    <button className='linkendin' ><FontAwesomeIcon icon={faLinkedin} />LinkedIn </button>
                 </a>
             </div>)
 }
